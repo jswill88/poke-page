cd6512ea04ec6f096d58781c780f9a897fb36a97
seanjmurray <seanjmurray@users.noreply.github.com>
2020-10-02 00:23:48 +0000

    deploy: ac4007149af9c51d89a926836324acf1cbd6099f

---
+++ precache-manifest.8d31e0e70de8592a1dde3c07a7207d3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca8e6f8c641d05656e9d4ad56f592af",
+    "url": "/401d37-redux-lab/poke-team/index.html"
+  },
+  {
+    "revision": "12edabebc97afd9f02f7",
+    "url": "/401d37-redux-lab/poke-team/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1d27c7bb77cfc3914fb6",
+    "url": "/401d37-redux-lab/poke-team/static/css/main.993337df.chunk.css"
+  },
+  {
+    "revision": "12edabebc97afd9f02f7",
+    "url": "/401d37-redux-lab/poke-team/static/js/2.934a7abe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/401d37-redux-lab/poke-team/static/js/2.934a7abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d27c7bb77cfc3914fb6",
+    "url": "/401d37-redux-lab/poke-team/static/js/main.9ffab424.chunk.js"
+  },
+  {
+    "revision": "ffecc562640e0a15bfa0",
+    "url": "/401d37-redux-lab/poke-team/static/js/runtime-main.3eddb1c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/poke-team/precache-manifest.242d1b301bba51e2125713ea64b3d314.js"
+  "/401d37-redux-lab/poke-team/precache-manifest.8d31e0e70de8592a1dde3c07a7207d3f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poke-team/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/401d37-redux-lab/poke-team/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
